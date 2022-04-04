e24ae0c9f345689e3b760eabc8e6157f0d35511f
kian <kmaranon9@gmail.com>
2022-04-04 03:22:32 -0700

    Removed box page file

--- pages/threejs/index.tsx
+++ pages/threejs/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import dynamic from 'next/dynamic'
 
-const Box = dynamic(() => import('./Box'),{
+const Box = dynamic(() => import('@/components/canvas/Box'),{
   ssr: false,
 })
 
