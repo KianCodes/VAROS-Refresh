489382eea37cbb9443b56ab822e5e8883d1b704c
kian <kmaranon9@gmail.com>
2022-04-04 04:02:51 -0700

    Added custom document file to add scripts, added custom Spline font, changed home page to navigate to three.js page

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style>
+            @import
+            url('https://fonts.googleapis.com/css2?family=Spline+Sans:wght@300;400;500;600;700&display=swap');
+          </style>
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
