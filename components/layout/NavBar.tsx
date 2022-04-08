ea14704d1eb86d89b63db333fee5a3a81d9fbaf7
Michael James Joseph <mjosep2@gmu.edu>
2022-04-08 11:36:21 -0400

    added animated video to home page and mission section with image

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import styles from "../../styles/Layout.module.scss"
+import Link from 'next/link'
 
 function NavBar() {
   return (
     <div className = {styles.navBar}>
         <ul className = {styles.navBarMenu}>
             <li className = {styles.navBarLogo}>
-                <a href="/">
-                    <img src="/images/TransparentLogo-DarkTheme.png" />
-                </a>
+                <Link href="/">
+                    <a>
+                        <img alt="VAROS Logo" src="/images/TransparentLogo-DarkTheme.png" />
+                    </a>
+                </Link>
             </li>
             <li className = {styles.navBarLinksWrapper}>
                 <ul className = {styles.navBarLinks}>
-                        <a href="/#mission">Mission</a>
-                        <a href="/#process">Process</a>
-                        <a href="/threejs">Demo</a>
-                        <a href="/#about">About</a>
+                    <Link href="/#mission"><a>Mission</a></Link>
+                    <Link href="/threejs"><a>Demo</a></Link>
+                    <Link href="/#about"><a>About</a></Link>
                 </ul>
             </li>
             <li className = {styles.navBarContact}>
-                <a href="/contact">Contact</a>
+                <Link href="/contact"><a>Contact</a></Link>
             </li>
         </ul>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import Head from 'next/head'
-
+import React, { useEffect,useState,useRef } from 'react'
 import styles from '../styles/Home.module.scss'
-import Link from 'next/link'
 
 export default function Home() {
+  const videoRef = useRef(null);
+  const [isIntersecting, setIntersecting] = useState(false)
+  const videoScrollRange = 349;
+
+  useEffect(() => {
+    const videoTimeRange = videoRef.current.duration;
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting)
+    )
+    
+    document.addEventListener('scroll', function(e) {
+      observer.observe(videoRef.current)
+
+      let scrollEvent = window.pageYOffset;
+      let videoTime = (scrollEvent/videoScrollRange)*videoTimeRange;
+      videoRef.current.currentTime = videoTime;
+    })
+  });
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,20 +33,45 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to{' '}
-          <a href="https://www.youtube.com/watch?v=q6EoRBvdVPQ">VAROS</a>
         </h1>
-
-        <div className={styles.grid}>
-          <Link href="/threejs">
-            <div className={styles.card}>
-              <h2>Three.js &rarr;</h2>
-              <p>Check out this cube bro</p>
-            </div>
-          </Link>
+        <img alt="VAROS Logo" src="/images/TransparentLogo-DarkTheme.png" />
+        <span className={styles.varos}>
+          <b className = {styles.vLight}>{"Virtual "}</b>
+          <b> {" & "}</b>
+          <b className = {styles.aRed} >{"Augmented "}</b>
+          <b className = {styles.rLight} >{" Reality "}</b>
+          <b className = {styles.oOrange} >{"Organ "}</b>
+          <b className = {styles.sRedWhite}>Systems</b>
+        </span>
+        <div className = {styles.videoMask}  >
+          <video width="650" ref={videoRef}>
+            <source src="/videos/VAROS_Liver_ANI.1.mp4" type="video/mp4"></source>
+          </video>
         </div>
       </main>
-
-      
+      <div id="mission" className={styles.mainMission}>
+        <img alt="Organs" src="/images/redOrgans.png" />
+        <div className={styles.missionWrapper}>
+          <h1 className={styles.subTitle}>
+            Mission
+          </h1>
+          <p>
+            Facilitate the success of patients, healthcare practitioners, 
+            and pharmaceutical companies through the use of innovative 
+            technology to improve health literacy.
+          </p>
+        </div>
+      </div>
+      <div id="process" className={styles.mainProcess}>
+        <h1 className={styles.subTitle}>
+          Process
+        </h1>
+      </div>
+      <div id="about" className={styles.mainTeam}>
+        <h1 className={styles.subTitle}>
+          Meet the team
+        </h1>
+      </div>
     </div>
   )
 }
