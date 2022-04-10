c709d8726a6b9f5c5549fc79663108dfd460a5ad
kian <kmaranon9@gmail.com>
2022-04-09 19:33:41 -0700

    Fixed width of elements to be responsive on smaller screens

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className={styles.layoutContainer}>
       <Header />
-        {children}
+      {children}
       <Footer />
     </div>
   )
