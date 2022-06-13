aa465dfbcd2b400ba5983f0c028648daa4ec5035
Michael James Joseph <mjosep2@gmu.edu>
2022-06-12 22:20:15 -0400

    added a 3d hdri background, added some better styling to the intestine and liver buttons, and got a material on the intestine. No luck getting a material on the liver

--- components/models/ModelOBJ.tsx
+++ components/models/ModelOBJ.tsx
@@ -2,10 +2,17 @@ import { useLoader } from '@react-three/fiber'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import React, { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
+import { MeshNormalMaterial, Mesh } from 'three'
 
 const ModelOBJ = ({ file }: { file: string }) => {
+  const mat = new MeshNormalMaterial()
   const obj = useLoader(OBJLoader, file)
   const ref = useRef<THREE.Mesh>(null!)
+  obj.traverse((child) => {
+    if(child instanceof Mesh) {
+      child.material = mat
+    }
+  })
 
   useFrame((state, delta) => (ref.current.rotation.y += 0.001))
 
--- pages/threejs/index.tsx
+++ pages/threejs/index.tsx
@@ -2,35 +2,32 @@ import React, { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import dynamic from 'next/dynamic'
 import ModelOBJ from '@/components/models/ModelOBJ'
-import { OrbitControls, Stage } from '@react-three/drei'
+import { OrbitControls, Stage, Environment } from '@react-three/drei'
+import styles from '../../styles/ThreeJS.module.scss'
 
 const Box = dynamic(() => import('@/components/canvas/Box'), {
   ssr: false,
 })
 
 const Three = () => {
-  let liverPath = '/models/VAROS-3D-Models/LiverHealthy.obj'
+  let liverPath = '/models/VAROS-3D-Models/LiverHealthy2.obj'
   let intestinePath = '/models/VAROS-3D-Models/VAROS_Intestine.obj'
   const [modelPath, setModelPath] = useState(liverPath)
   const handleClick = (event) => {
     setModelPath(event.target.value)
   }
+
   return (
-    <div>
-      <button onClick={handleClick} value={intestinePath}>
-        Intestine
-      </button>
-      <button onClick={handleClick} value={liverPath}>
-        Liver
-      </button>
-      <div
-        style={{
-          width: 1000,
-          height: 1000,
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-      >
+    <div className = {styles.container}>
+      <div className = {styles.buttons}>
+        <button onClick={handleClick} value={intestinePath}>
+          Intestine
+        </button>
+        <button onClick={handleClick} value={liverPath}>
+          Liver
+        </button>
+      </div>
+      <div className = {styles.main}>
         <Canvas>
           <OrbitControls
             enablePan={true}
@@ -41,7 +38,7 @@ const Three = () => {
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <ModelOBJ file={modelPath} />
-            {/* <Box /> */}
+            <Environment files = 'nebula n0.hdr' path={'/backgrounds/'} background />
           </Stage>
         </Canvas>
       </div>
